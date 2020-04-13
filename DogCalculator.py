e07bf20b8d3ed7a5bef0e7fc1f518ab957a71426
Raman Heera <rheera2@uwo.ca>
2020-04-13 19:23:43 -0400

    Updated Dog age to Human age function
    - fixed my excel function so that the x-intercept was at 0 which fixed the function

--- DogCalculator.py
+++ DogCalculator.py
@@ -1,5 +1,4 @@
-import math
-from datetime import date, timedelta
+from datetime import date
 
 
 def calculate_age_days():
@@ -15,15 +14,18 @@ def calculate_age_days():
 
 def dog_to_human(dog_age):
     """Takes dogs age in days as input and converts it into the equivalent human age in days"""
+    # todo 0 to 6 function isn't working properly need a better one
     # ln(x) = log(x, e)
     # human_age = (16 * (math.log(int(dog_age), math.e))) + 31 known best dog age formula
-    # y = 8E-15x6 - 6E-11x5 + 1E-07x4 - 0.0002x3 + 0.0764x2 + 2.3013x - 16.923 equation for 0 - 6 years old
-    # y = 4.5x + 5566.3 equation for 6 - 30 years old
+    # y = 8E-15x6 - 6E-11x5 + 1E-07x4 - 0.0002x3 + 0.0773x2 + 2.0835x equation for 0 - 6 years old
+    # y = 4.5x + 5566.3 equation for 6 - 30 years old (picked 30 years since oldest dog ever is 29)
     if dog_age < 0:
         print("Dog age must be greater than 0")
         return -1
-    elif dog_age > 0 and dog_age < 2191.455:
-        human_age = ((8 * (10 ** -15)) * (float(dog_age) ** 6)) - ((6 * (10 ** -11)) * (float(dog_age) ** 5)) + ((1 * (10 ** -7)) * (float(dog_age) ** 4)) - (0.0002 * (float(dog_age) ** 3)) + (0.0764 * (float(dog_age) ** 2)) + (2.3013 * (float(dog_age))) - 16
+    elif dog_age >= 0 and dog_age < 2191.455:
+        human_age = ((8 * (10 ** -15)) * (float(dog_age) ** 6)) - ((6 * (10 ** -11)) * (float(dog_age) ** 5)) + \
+                    ((1 * (10 ** -7)) * (float(dog_age) ** 4)) - (0.0002 * (float(dog_age) ** 3)) + \
+                    (0.0773 * (float(dog_age) ** 2)) + (2.0835 * (float(dog_age)))
         return human_age
     elif dog_age >= 2191.455 and dog_age < 10957.275:
         human_age = (4.5 * float(dog_age)) + 5566.3
@@ -31,6 +33,12 @@ def dog_to_human(dog_age):
     else:
         print("Dog age must be less than 30 years old")
         return -1
+
+
+def dog_human_intersect(human_age, dog_age):
+    human_minus_dog_age = human_age - dog_age
+    intersect = (human_age - dog_age) / ()
+
 
 #print(dog_to_human())
 #print(dog_date_to_years())
