9cd8726652f339fff774a6aaba2139f33095c96a
Raman Heera <rheera2@uwo.ca>
2020-04-15 10:28:17 -0400

    Calculations
    - got age calculations cleaned up
    - Got the intersect a new equation to work with all ages, working well so far
    - set up a print for human and dogs intersectting age

--- DogCalculator.py
+++ DogCalculator.py
@@ -1,14 +1,17 @@
 from datetime import date, timedelta
 from numpy import roots, polyval
 
-def calculate_age_days():
-    """Takes user input for day, month, and year and returns the users age in days"""
+def date_input():
     birthday = input()
     birthmonth = input()
     birthyear = input()
     born = date(int(birthyear), int(birthmonth), int(birthday))
+    return born
+
+def calculate_age_days(year, month, day):
+    """Takes user input for day, month, and year and returns the users age in days"""
     today = date.today()
-    age = today - born
+    age = today - date(int(year), int(month), int(day))
     return age.days
 
 
@@ -17,17 +20,24 @@ def dog_to_human(dog_age):
     # todo 0 to 6 function isn't working properly need a better one
     # ln(x) = log(x, e)
     # human_age = (16 * (math.log(int(dog_age), math.e))) + 31 known best dog age formula
-    # y = 8E-15x6 - 6E-11x5 + 1E-07x4 - 0.0002x3 + 0.0773x2 + 2.0835x equation for 0 - 6 years old
+    # y = 0.0441x2 + 2.5382x equation for 0 - 1 years old
+    # y = -0.0068x2 + 16.868x equation for 1 - 4 years old
+    # y = -0.0018x2 + 10.857x equation for 3 - 6 years old
     # y = -0.0001x2 + 6.2952x equation for 6 - 30 years old (picked 30 years since oldest dog ever is 29)
     if dog_age < 0:
         print("Dog age must be greater than 0")
         return -1
-    elif dog_age >= 0 and dog_age < 2191.455:
-        human_age = ((8 * (10 ** -15)) * (float(dog_age) ** 6)) - ((6 * (10 ** -11)) * (float(dog_age) ** 5)) + \
-                    ((1 * (10 ** -7)) * (float(dog_age) ** 4)) - (0.0002 * (float(dog_age) ** 3)) + \
-                    (0.0773 * (float(dog_age) ** 2)) + (2.0835 * (float(dog_age)))
+    elif 0 <= dog_age < 243.5:
+        human_age = (0.0441 * (float(dog_age) ** 2)) + (2.5382 * float(dog_age))
+        # returning 1 since it is in the first age grouping
         return human_age
-    elif dog_age >= 2191.455 and dog_age < 10957.275:
+    elif 243.5 <= dog_age < 1095.7275:
+        human_age = (-0.0068 * (float(dog_age) ** 2)) + (16.868 * float(dog_age))
+        return human_age
+    elif 1095.7275 <= dog_age < 2191.455:
+        human_age = (-0.0018 * (float(dog_age) ** 2)) + (10.857 * float(dog_age))
+        return human_age
+    elif 2191.455 <= dog_age < 10957.275:
         human_age = (-0.0001 * (float(dog_age) ** 2)) + (6.2952 * float(dog_age))
         return human_age
     else:
@@ -40,40 +50,57 @@ def x_vertex(a, b):
 
 
 def dog_human_intersect(human_age, dog_human_age):
+    # todo make a check to make sure humans age is greater than the dogs
+    # todo for age less than 1 the parabola is going upwards so the root cannot be greater than 0 and be less than the vert
+    # dog_in_human_age + dog_to_human(x) = human_age + x
+    # x is the number of human years it will take for both dog and human to be the same age
     # polynomial ax^2 + bx + c
-    # a
+    # y = -0.0001x2 + 6.4551x
     a = -0.0001
-    b = 5.2952
+    b = 6.4551
     c = human_age - dog_human_age
     # our equation has ax^2 + bx = c so we need to do -c
-    xroots = roots([-0.0001, 5.2952, -c])
+    xroots = roots([a, b - 1, -c])
     # the only root we want is the x value before the peak of the parabola
     xvert = x_vertex(a, b)
     for root in xroots:
-        if root < xvert:
+        if xvert > root > 0:
             return root
         else:
             continue
     return -1
+
 
 def dog_human_date(intersect, human_age, dog_age):
     today = date.today()
     same_age_date = today + timedelta(days=intersect)
     dog_new_age = dog_age + intersect
     human_new_age = human_age + intersect
-    print("Your dog will be ", dog_new_age/365.2425, " years old")
-    print("You will be ", human_new_age/365.2425, " years old")
+    human_new_age_years = human_new_age/365.2425
+    dog_new_age_years = dog_new_age/365.2425
+    print(human_new_age_years)
+    print(dog_new_age_years)
+    if round(dog_new_age_years) > dog_new_age_years:
+        print("Your dog will almost be", round(dog_new_age_years), " years old")
+    else:
+        print("Your dog will be ", round(dog_new_age_years), " years old")
+    if round(human_new_age_years) > human_new_age_years:
+        print("You will be almost", round(human_new_age_years), " years old")
+    else:
+        print("You will be", round(human_new_age_years), " years old")
     return same_age_date
 
 #print(dog_to_human())
 #print(dog_date_to_years())
-dog_age = calculate_age_days()
+dog_age = calculate_age_days(2020, 3, 15)
 print(dog_age)
 dog_human_age = dog_to_human(dog_age)
 print(dog_human_age)
-dog_human_age2 = dog_to_human(3805)
-print(dog_human_age2)
-intersect = dog_human_intersect(21900, dog_human_age)
+# dog_human_age2 = dog_to_human(3805)
+# print(dog_human_age2)
+print("You are ", 16790/365.2425, " years old")
+print("Your dog is  ", dog_age/365.2425, " years old")
+intersect = dog_human_intersect(16790, dog_human_age)
 print(intersect)
-human_same_age = dog_human_date(intersect, 21900, dog_age)
+human_same_age = dog_human_date(intersect, 16790, dog_age)
 print(human_same_age)
